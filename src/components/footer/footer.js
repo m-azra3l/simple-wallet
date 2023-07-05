8c80b916a10e7e5e65cbce656fc23d8a16f3104e
Michael <gadreelazazel@gmail.com>
2023-07-05 23:35:22 +0100

    Update

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -10,8 +10,8 @@ const Footer = () => {
       <div className={styles.container}>
         {/* <div>By AZRA3L. All rights reserved. &nbsp;</div> */}
         <div className={styles.social}>
-          <Link href={'https://github.com/m-azra3l/simple-wallet'} target="_blank"><Image src="/github.jpg" width={35} height={35} className={styles.icon} alt="Github" /></Link>
-          <Link href={'https://www.linkedin.com/in/michael-damilare-adesina-4b51a5134/'} target="_blank"><Image src="/linkedin.png" width={40} height={40} className={styles.icon} alt="LinkedIn" /></Link>
+          <Link href={'https://github.com/m-azra3l/simple-wallet'} title="Repository"  target="_blank"><Image src="/github.jpg" width={35} height={35} className={styles.icon} alt="Github" /></Link>
+          <Link href={'https://www.linkedin.com/in/michael-damilare-adesina-4b51a5134/'} title="LinkedIn"  target="_blank"><Image src="/linkedin.png" width={40} height={40} className={styles.icon} alt="LinkedIn" /></Link>
         </div>
       </div>
     </>
